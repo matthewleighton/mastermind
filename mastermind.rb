c54ae60eff9e395446747e8b875192db7bf1b368
Matthew <matthewrleighton@gmail.com>
2015-11-18 10:35:18 +0000

    refactored valid_guess?/valid_code? together to valid_entry?

--- mastermind.rb
+++ mastermind.rb
@@ -3,6 +3,7 @@ module Game
 
 		def initialize
 			@code = generate_code
+			@guess = []
 		end
 
 		def generate_code
@@ -21,28 +22,23 @@ module Game
 		def new_game
 			reset_variables
 			choose_role
-			@player_role == "1" ? player_guessing_loop : computer_guessing_loop
+			if @player_role == "guess"
+				player_guessing_loop
+			else
+				enter_code
+				computer_guessing_loop
+			end
 		end
 
 		def choose_role
 			puts "Would you like to be the one making or guessing the code?"
 			puts "[1] - Guess the code\n[2] - Make the code"
-			@player_role = gets.chomp.upcase[0]
-			until @player_role == "1" || @player_role == "2"
+			player_role = gets.chomp.upcase[0]
+			until player_role == "1" || player_role == "2"
 				puts "Sorry, please enter either 1 or 2.\n[1] - Guess the code\n[2] - Make the code"
-				@player_role = gets.chomp.upcase[0]
+				player_role = gets.chomp.upcase[0]
 			end
-		end
-
-		def player_guessing_loop
-			while game_continues?
-				turn
-			end
-			correct_guess? ? guesser_wins : guesser_loses
-			new_game if play_again?
-		end
-
-		def computer_guessing_loop
+			player_role == "1" ? @player_role = "guess" : @player_role = "make"
 		end
 
 		def reset_variables
@@ -61,14 +57,24 @@ module Game
 			puts "y: The number of digits in the wrong position.\n "
 		end
 
+# ------- Player Guessing --------------
+
+		def player_guessing_loop
+			while game_continues?
+				turn
+			end
+			correct_guess? ? guesser_wins : guesser_loses
+			new_game if play_again?
+		end
+
 		def turn
 			puts @remaining_turns == 1 ? "\n#{@remaining_turns} guess remaining." : "\n#{@remaining_turns} guesses remaining."
 			@remaining_turns -= 1
 			puts "Enter your guess:"
 			@guess = gets.chomp.chars.map! {|x| x.to_i}
 			puts ""
-			until valid_guess?
-				enter_valid_guess
+			until valid_entry?
+				enter_valid_entry
 			end
 			unless correct_guess?
 				calculate_feedback
@@ -77,18 +83,26 @@ module Game
 			end
 		end
 
-		def valid_guess?
-			if (@guess.count != 4) || !(@guess.all? { |x| (1..6).include? x.to_i })
+# -------- Entry validation ----------------
+
+		def valid_entry?
+			p @code # TEST LINE
+			@player_role == "guess" ? entry = @guess : entry = @code
+			if entry.count != 4 || !(entry.all? { |x| (1..6).include? x.to_i } )
 				false
 			else
 				true
 			end
 		end
 
-		def enter_valid_guess
-			puts "Please enter a valid guess. (4 digits between 1 and 6)"
-			@guess = gets.chomp.chars.map! {|x| x.to_i}
+		def enter_valid_entry
+			@player_role == "guess" ? word = "guess" : word = "code"
+			puts "Please enter a valid #{word}. (4 digits between 1 and 6)."
+			entry = gets.chomp.chars.map! {|x| x.to_i}
+			@player_role == "guess" ? @guess = entry : @code = entry
 		end
+
+# --------------------------------------------		
 
 		def game_continues?
 			(@remaining_turns > 0) && !(correct_guess?) ? true : false
@@ -148,6 +162,22 @@ module Game
 			end
 			answer == "Y" ? true : false
 		end
+
+		# ------------- Computer Guessing -------------
+
+		def enter_code
+			puts "Enter a code for the computer to guess. (4 digits between 1 and 6)."
+			@code = gets.chomp.chars.map! {|x| x.to_i}
+			until valid_entry?
+				enter_valid_entry
+			end
+			p "woop!"
+		end
+
+		def computer_guessing_loop
+
+		end
+
 
 	end # End of Mastermind class
 end # End of Game module
